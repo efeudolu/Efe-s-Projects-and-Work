8d56dbd589b709dd287ebee4049d2795a9197b63
Efe Udolu <52173328+efeudolu@users.noreply.github.com>
2023-10-09 19:08:23 -0600

    Delete Give Me A Discount.py

--- Give Me A Discount.py
+++
@@ -1,6 +0,0 @@
-def give_me_a_discount(full_price, discount):
-    discounted = full_price * ((1-discount/100))
-    print("The cost with a discount will be : " + str(discounted))
-
-
-give_me_a_discount(100, 60)
