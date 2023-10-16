bba9f41e5b7d9b7eaaa49ee41195415dd31188e8
Efe Udolu <efeudolu107@gmail.com>
2023-10-15 19:43:50 -0600

    Update 2013-2014 UCL Goals and Assists Analysis.py

--- 2013-2014 UCL Stats Full Analysis/2013-2014 UCL Goals and Assists Analysis.py
+++ 2013-2014 UCL Stats Full Analysis/2013-2014 UCL Goals and Assists Analysis.py
@@ -17,8 +17,8 @@ def create_bar_chart(x_values, y_values, x_label, y_label, title, number_of_tick
     plt.show()
 
 
-players = ["Ronaldo", "Ibrahimovic & Bale & Benzema", "Costa"]
-goals_and_assists = [22, 10, 9]
+players = ["Ronaldo", "Ibrahimovic & Bale & Benzema", "Costa & Di Maria", "Rooney & Robben & Aguero & Lewandowksi & Messi"]
+goals_and_assists = [22, 10, 9, 8]
 ticks = list(range(1, max(goals_and_assists) + 1))
 
 create_bar_chart(players, goals_and_assists, 'Player Names', 'Goals & Assists (Direct Goal Contributions)', 'Direct Goal Contribution Leaders of 2013-2014 UCL Season', ticks)
